523d2800d6c83c8104299a726faca1d35eeb5042
Ivan Lelikov <critica1989@gmail.com>
2015-10-17 00:09:11 +0800

    second

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,41 @@
-global.hostname = "localhost";
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var opn = require('opn');
 
-var gulp = require('gulp'),
-sass = require('gulp-sass'),
-autoprefixer = require('gulp-autoprefixer'),
-minifycss = require('gulp-minify-css'),
-rename = require('gulp-rename');
-
-gulp.task('express', function() {
-	var express = require('express');
-	var app = express();
-	app.use(require('connect-livereload')({port: 35729}));
-	app.use(express.static(__dirname + '/app'));
-	app.listen('80', hostname);
+// Сервер с лайврилоадом
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true,
+    port:888
+  });
+  opn('http://localhost:8888')
 });
 
-var tinylr;
-gulp.task('livereload', function() {
-	tinylr = require('tiny-lr')();
-	tinylr.listen(35729);
+//Работа с html
+gulp.task('html',function(){
+	gulp.src('./app/*.html')
+	.pipe(connect.reload());
 });
 
-function notifyLiveReload(event) {
-	var fileName = require('path').relative(__dirname, event.path);
-	tinylr.changed({
-		body: {
-			files: [fileName]
-		}
-	});
-}
-
-gulp.task('styles', function () {
-	gulp.src('sass/*.sass')
-	.pipe(sass({
-		includePaths: require('node-bourbon').includePaths
-	}).on('error', sass.logError))
-	.pipe(rename({suffix: '.min', prefix : '_'}))
-	.pipe(autoprefixer({
-		browsers: ['last 15 versions'],
-		cascade: false
-	}))
-	.pipe(minifycss())
-	.pipe(gulp.dest('app'));
+//Работа с css
+gulp.task('css',function(){
+	gulp.src('./app/css/*.css')
+	.pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-	gulp.watch('sass/*.sass', ['styles']);
-	gulp.watch('app/*.css', notifyLiveReload);
-	gulp.watch('app/*.html', notifyLiveReload);
+//Работа с js
+gulp.task('js',function(){
+	gulp.src('./app/js/*.js')
+	.pipe(connect.reload());
 });
 
-gulp.task('default', ['styles', 'express', 'livereload', 'watch'], function() {
+gulp.task('watch',function(){
+	gulp.watch(['./app/*.html'], ['html']),
+	gulp.watch(['./app/css/*.css'], ['css']),
+	gulp.watch(['./app/js/*.js'], ['js']);
+});
+// Задача по-умолчаню
+gulp.task('default', ['connect','watch'], function() {
 
 });
